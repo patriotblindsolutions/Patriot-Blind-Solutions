6dab03579f9dc82aa8acf560e9464e330d003078
brojonbeast <114700675+brojonbeast@users.noreply.github.com>
2026-04-07 12:11:57 -0500

    Update LinkedIn link in footer with correct company page URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -160,7 +160,7 @@ export default function Home() {
                             <img alt="Patriot Blind Solutions" className="h-12 w-auto mx-auto" src="/logo.png" />
                             <p className="text-gray-300 text-sm leading-6">The nationwide leader in industrial Blinding Management Systems. Providing critical positive isolation support for large-scale refinery and chemical plant turnarounds.</p>
                             <div className="flex justify-center space-x-6">
-                                <a className="text-gray-400 hover:text-white" href="#">
+                                <a className="text-gray-400 hover:text-white" href="https://www.linkedin.com/company/patriot-blind-solutions/about/">
                                     <span className="sr-only">LinkedIn</span>
                                     <svg aria-hidden="true" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                                         <path clipRule="evenodd" d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" fillRule="evenodd"></path>
